7bc7dc203dab70d12d42692c56b9bc84914a552f
Sylvain Reucherand <sreucherand@users.noreply.github.com>
2016-04-10 16:17:13 +0200

    Avoid setting display block when an element is about to be shown again (#2)
    * Prevent set display block on specific display element when shown
    * Add tests

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = shim
 
-function shim(elem, value) {
+function shim (element, value) {
     if (value === undefined) {
-        return elem.style.display === "none"
+        return element.style.display === 'none'
     }
 
-    elem.style.display = value ? "none" : "block"
+    element.style.display = (value || null) && 'none'
 }
